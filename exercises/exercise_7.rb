1eb0dd5f3a3c6d348da2cfbc545380ec78b3d6db
Catherine Mitchell <nicholscat@yahoo.com>
2023-05-02 13:50:08 -0300

    added validations to two models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,23 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+end
+
+class Employees < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, great_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates :belongs_to, presence: true
+end
+
+class Stores < ApplicationRecord
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+end
+  puts @store4 = Store.find_by(name: "Richmond")
+
+
+Store.create(name: "Chilliwack")
