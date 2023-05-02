109376a13ad3b1e837b71a29dd68d5c10279c42b
Catherine Mitchell <nicholscat@yahoo.com>
2023-05-02 13:49:07 -0300

    add has_many :employees directly inside the Store model, added belongs_to :store directly inside the Employee model.  Added some data into employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Catherine", last_name: "Mitchell", hourly_rate: 75)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 75)
+@store2.employees.create(first_name: "Jim", last_name: "Doe", hourly_rate: 80)
+
+puts @store1
